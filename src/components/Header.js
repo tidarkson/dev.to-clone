43b71b704fd43592e64505a1867a3cf9cac58a70
Tingwutobong Okonson <tidarkson@gmail.com>
2023-03-17 19:33:52 +0100

    commit message

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,17 +2,17 @@ import React from 'react'
 
 export default function Header() {
     return (
-        <div>
-            <header className='max-w-100% flex items-center justify-between'>
-                <div className='flex items-center justify-start'>
+        <>
+            <header className='max-w-xl mx-auto flex items-center justify-between'>
+                <div className='flex items-center justify-start ml-4'>
                     <img src="./image/dev.png" alt="dev" className='w-12 mr-2' />
                     <input type="text" placeholder='Search...' className='h-10 w-70 border-2 rounded p-2 border-slate-300 outline-indigo-700' />
                 </div>
-                <div className='flex items-center justify-start gap-3 invisible sm:visible' >
+                <div className='flex items-center justify-start gap-3 invisible md:visible' >
                     <button className='login hover:bg-indigo-200 py-2 px-4 rounded-lg '>Log in</button>
                     <button className='border border-indigo-600 hover:bg-indigo-700 text-indigo-700 hover:text-white py-2 px-4 rounded-lg'>Create account</button>
                 </div>
             </header>
-        </div>
+        </>
     )
 }
